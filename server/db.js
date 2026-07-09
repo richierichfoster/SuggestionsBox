612bec59d64939d6474194ef42b9f62de583a1ed
richierichfoster <richie@fixitrightplumbing.com.au>
2026-07-09 15:08:07 +1000

    fix data path for volume

--- server/db.js
+++ server/db.js
@@ -5,7 +5,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const file = path.join(__dirname, 'data.json');
+const dataDir = process.env.RAILWAY_VOLUME_MOUNT_PATH || __dirname;
+const file = path.join(dataDir, 'data.json');
 const adapter = new JSONFile(file);
 
 const defaultData = {
